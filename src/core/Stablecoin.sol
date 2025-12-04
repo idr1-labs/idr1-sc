100b9e5f223997fa351849993398a708beb06a4d
Lagxy <lexysamuel1607@gmail.com>
2025-12-04 15:22:26 +0700

    template

---
+++ src/core/Stablecoin.sol
@@ -0,0 +1,41 @@
+// SPDX-License-Identifier: MIT
+// Compatible with OpenZeppelin Contracts ^5.5.0
+pragma solidity ^0.8.27;
+
+import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import {ERC20Burnable} from "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
+import {ERC20Pausable} from "@openzeppelin/contracts/token/ERC20/extensions/ERC20Pausable.sol";
+import {ERC20Permit} from "@openzeppelin/contracts/token/ERC20/extensions/ERC20Permit.sol";
+import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
+
+// import {Verifier}
+
+contract Stablecoin is ERC20, ERC20Burnable, ERC20Pausable, Ownable, ERC20Permit {
+    // Verifier public verifier;
+
+    constructor(address initialOwner) ERC20("Stablecoin", "STCN") Ownable(initialOwner) ERC20Permit("Stablecoin") {}
+
+    function pause() public onlyOwner {
+        _pause();
+    }
+
+    function unpause() public onlyOwner {
+        _unpause();
+    }
+
+    function mint(address to, uint256 amount, bytes calldata proof, bytes32 nullifier, uint256[] calldata publicInputs)
+        public
+        onlyOwner
+    {
+        // verify
+
+        // mint
+        _mint(to, amount);
+    }
+
+    // The following functions are overrides required by Solidity.
+
+    function _update(address from, address to, uint256 value) internal override(ERC20, ERC20Pausable) {
+        super._update(from, to, value);
+    }
+}
